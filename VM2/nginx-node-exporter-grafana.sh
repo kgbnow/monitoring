91150496af655f725a7329f71b0d547a298920d9
Kirill Gubanov <gubanof.kiril@yandex.ru>
2023-05-27 15:03:07 +0300

    update

--- VM2/nginx-node-exporter-grafana.sh
+++ VM2/nginx-node-exporter-grafana.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt install nginx
-mv ./default /etc/nginx/sites-enabled/
+apt update
+apt install nginx -y
+mv ./default /etc/nginx/sites-available/
 wget https://github.com/nginxinc/nginx-prometheus-exporter/releases/download/v0.11.0/nginx-prometheus-exporter_0.11.0_linux_amd64.tar.gz
 wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 tar -xf nginx-prometheus-exporter_0.11.0_linux_amd64.tar.gz
@@ -12,6 +13,7 @@ wget https://dl.grafana.com/oss/release/grafana_9.5.2_amd64.deb
 dpkg -i grafana_9.5.2_amd64.deb
 mv ./node-exporter.service /etc/systemd/system/
 mv ./nginx-exporter.service /etc/systemd/system/
+rm grafana_9.5.2_amd64.deb
 systemctl daemon-reload
 systemctl enable grafana-server
 systemctl enable node-exporter.service
