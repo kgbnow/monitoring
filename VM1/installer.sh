fa17b72b5f5ee7e1b408049b847eb5542be53ea6
Kirill Gubanov <gubanof.kiril@yandex.ru>
2023-05-30 18:56:45 +0300

    i hope this is last vesion

--- VM1/installer.sh
+++ VM1/installer.sh
@@ -15,12 +15,27 @@ download_and_extract() {
     local url="$1"
     local filename="$2"
 
-    wget -q "$url" -O "$TMP_DIR/$filename"
+    # Загрузка файла
+    if ! wget -q "$url" -O "$TMP_DIR/$filename"; then
+        echo "Ошибка при загрузке файла: $url"
+        return 1
+    fi
 
-    tar -xf "$TMP_DIR/$filename" -C "$TMP_DIR"
+    # Разархивирование файла
+    if ! tar -xf "$TMP_DIR/$filename" -C "$TMP_DIR"; then
+        echo "Ошибка при разархивировании файла: $filename"
+        return 1
+    fi
 
     local dirname="${filename%.tar.gz}"
-    cp -r "$TMP_DIR/$dirname" "$INSTALL_DIR"
+    local source_dir="$TMP_DIR/$dirname"
+    local destination_dir="$INSTALL_DIR/$dirname"
+
+    # Копирование директории
+    if ! cp -r "$source_dir" "$destination_dir"; then
+        echo "Ошибка при копировании директории: $source_dir"
+        return 1
+    fi
 }
 
 # Функция проверки наличия сервиса
@@ -29,26 +44,30 @@ service_exists() {
     systemctl is-enabled "$service_name" >/dev/null 2>&1
 }
 
-# Функция обновления сервиса
+# Функция обновления сервис-файла
 update_service() {
     local service_name="$1"
     local service_file="$2"
     local version_value="$3"
 
     if service_exists "$service_name"; then
-        echo "Обновление сервиса $service_name"
-        systemctl stop "$service_name"
-        systemctl disable "$service_name"
-        rm "/etc/systemd/system/$service_file"
+        echo "Обновление файла сервиса $service_name"
+        systemctl stop "$service_name" || { echo "Ошибка при остановке сервиса $service_name"; return 1; }
+        systemctl disable "$service_name" || { echo "Ошибка при отключении сервиса $service_name"; return 1; }
+        rm "/etc/systemd/system/$service_file" || { echo "Ошибка при удалении сервис-файла $service_file"; return 1; }
     fi
 
     # Замена версии в сервис-файле
-    sed "s/$VERSION_PLACEHOLDER/$version_value/g" "$service_file" > "/etc/systemd/system/$service_file"
+    sed "s/$VERSION_PLACEHOLDER/$version_value/g" "$service_file" > "/etc/systemd/system/$service_file" || { echo "Ошибка при замене версии в сервис-файле $service_file"; return 1; }
 
     systemctl daemon-reload
-    systemctl enable "$service_name"
-    systemctl start "$service_name"
+    systemctl enable "$service_name" || { echo "Ошибка при включении сервиса $service_name"; return 1; }
+    systemctl start "$service_name" || { echo "Ошибка при запуске сервиса $service_name"; return 1; }
 }
+
+
+# Проверка наличия директории Prometheus с нужной версией в папке установки
+if [ ! -d "$INSTALL_DIR/prometheus-$PROMETHEUS_VERSION.linux-amd64" ]; then
 
 # Загрузка и установка Prometheus
 prometheus_url="https://github.com/prometheus/prometheus/releases/download/v$PROMETHEUS_VERSION/prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz"
@@ -62,6 +81,21 @@ cp "$CONFIG_DIR/alert.rules.yml" "$INSTALL_DIR/prometheus-$PROMETHEUS_VERSION.li
 # Обновление сервиса Prometheus
 update_service "prometheus.service" "./prometheus.service" "$PROMETHEUS_VERSION"
 
+ # Проверка наличия другой версии Prometheus в папке установки
+    other_version_dir=$(find "$INSTALL_DIR" -maxdepth 1 -type d -name "prometheus-*" | grep -v "$PROMETHEUS_VERSION")
+    if [ -n "$other_version_dir" ]; then
+        # Удаление другой версии Prometheus из папки установки
+        echo "Удаление предыдущей версии Prometheus: $other_version_dir"
+        rm -rf "$other_version_dir"
+    fi
+
+else
+    echo "Директория Prometheus с версией $PROMETHEUS_VERSION уже существует в папке установки. Пропуск загрузки и установки."
+fi
+
+# Проверка наличия директории Alertmanager с нужной версией в папке установки
+if [ ! -d "$INSTALL_DIR/alertmanager-$ALERTMANAGER_VERSION.linux-amd64" ]; then
+
 # Загрузка и установка Alertmanager
 alertmanager_url="https://github.com/prometheus/alertmanager/releases/download/v$ALERTMANAGER_VERSION/alertmanager-$ALERTMANAGER_VERSION.linux-amd64.tar.gz"
 alertmanager_filename="alertmanager-$ALERTMANAGER_VERSION.linux-amd64.tar.gz"
@@ -73,6 +107,21 @@ cp "$CONFIG_DIR/alertmanager.yml" "$INSTALL_DIR/alertmanager-$ALERTMANAGER_VERSI
 # Обновление сервиса Alertmanager
 update_service "alertmanager.service" "./alertmanager.service" "$ALERTMANAGER_VERSION"
 
+ # Проверка наличия другой версии Alertmanager в папке установки
+    other_version_dir=$(find "$INSTALL_DIR" -maxdepth 1 -type d -name "alertmanager-*" | grep -v "$ALERTMANAGER_VERSION")
+    if [ -n "$other_version_dir" ]; then
+        # Удаление другой версии Alertmanager из папки установки
+        echo "Удаление предыдущей версии Alertmanager: $other_version_dir"
+        rm -rf "$other_version_dir"
+    fi
+
+else
+    echo "Alertmanager с версией $ALERTMANAGER_VERSION уже существует в папке установки. Пропуск загрузки и установки."
+fi
+
+# Проверка наличия директории Node Exporter с нужной версией в папке установки
+if [ ! -d "$INSTALL_DIR/node_exporter-$NODE_EXPORTER.linux-amd64" ]; then
+
 # Загрузка и установка Node Exporter
 node_exporter_url="https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz"
 node_exporter_filename="node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz"
@@ -80,6 +129,18 @@ download_and_extract "$node_exporter_url" "$node_exporter_filename"
 
 # Обновление сервиса Node Exporter
 update_service "node-exporter.service" "./node-exporter.service" "$NODE_EXPORTER_VERSION"
+
+ # Проверка наличия другой версии Node Exporter в папке установки
+    other_version_dir=$(find "$INSTALL_DIR" -maxdepth 1 -type d -name "node_exporter-*" | grep -v "$NODE_EXPORTER_VERSION")
+    if [ -n "$other_version_dir" ]; then
+        # Удаление другой версии Node Exporter из папки установки
+        echo "Удаление предыдущей версии Node Exporter: $other_version_dir"
+        rm -rf "$other_version_dir"
+    fi
+
+else
+    echo "Node Exporter с версией $NODE_EXPORTER_VERSION уже существует в папке установки. Пропуск загрузки и установки."
+fi
 
 # Удаление временной директории
 rm -r "$TMP_DIR"
